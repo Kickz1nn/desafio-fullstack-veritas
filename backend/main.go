0993cfb7418fd3efc2a2043e17f38f11a3917fbc
KickDown <oliveiradacruzcaio@gmail.com>
2026-08-12 17:19:34 -0300

    feat: add CORS middleware

--- backend/main.go
+++ backend/main.go
@@ -4,15 +4,17 @@ import (
 	"fmt"
 	"net/http"
 
+	"desafio-fullstack-veritas/backend/middleware"
 	"desafio-fullstack-veritas/backend/routes"
 )
 
 func main() {
 	router := routes.SetupRoutes()
+	handler := middleware.CORS(router)
 
 	fmt.Println("Servidor rodando em http://localhost:8080")
 
-	err := http.ListenAndServe(":8080", router)
+	err := http.ListenAndServe(":8080", handler)
 
 	if err != nil {
 		fmt.Println("Erro ao inicializar o servidor: ", err)
